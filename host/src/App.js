75f2ae5d88c3538667040c70ef650abeb5614cb0
Jan Deinhard <jan.deinhard@gmail.com>
2023-03-14 22:45:57 +0100

    remove RemoteWrapper

--- host/src/App.js
+++ host/src/App.js
@@ -4,28 +4,18 @@ import ErrorBoundary from "./ErrorBoundary";
 const RemoteApp = React.lazy(() => import("Remote/App"));
 const RemoteButton = React.lazy(() => import("Remote/Button"));
 
-const RemoteWrapper = ({ children }) => (
-  <div
-    style={{
-      border: "1px solid red",
-      background: "white",
-    }}
-  >
-    <ErrorBoundary>{children}</ErrorBoundary>
-  </div>
-);
-
 export const App = () => (
-  <div style={{ background: "rgba(43, 192, 219, 0.3)" }}>
+  <div>
     <h1>This is the Host!</h1>
     <h2>Remote App:</h2>
-    <RemoteWrapper>
+    <ErrorBoundary>
       <RemoteApp />
-    </RemoteWrapper>
+    </ErrorBoundary>
     <h2>Remote Button:</h2>
-    <RemoteWrapper>
+    <ErrorBoundary>
       <RemoteButton />
-    </RemoteWrapper>
+    </ErrorBoundary>
+    <br />
     <br />
     <a href="http://localhost:4000">Link to Remote App</a>
   </div>
